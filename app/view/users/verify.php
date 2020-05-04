339121493a2a69898579dfaecebbee868c70ca39
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-05-04 04:57:07 -0400

    aesthetics

--- app/view/users/verify.php
+++ app/view/users/verify.php
@@ -1,4 +1,4 @@
-<main role="main" class="container">
+<main role="main" class="container text-center">
 
     <nav aria-label="breadcrumb" class="mt-2">
         <ol class="breadcrumb">
@@ -8,22 +8,28 @@
         </ol>
     </nav>
 
-    <h1>Users</h1>
-    <h3>Verify a user</h3>
+    <div class="container text-center">
+        <div class="row justify-content-md-center">
+            <div class="col-5">
+                <h1>Verify</h1>
 
-    <?php 
-        if (isset($result)) { 
-            $code = explode(' ',trim($result));
-    ?>
-        <div class="alert alert-primary" role="alert"><?php echo $result; ?></div>
-    <?php } ?>
+                <?php 
+                    if (isset($result)) { 
+                        $code = explode(' ',trim($result));
+                        //logged
+                ?>
+                    <div class="alert alert-primary" role="alert"><?php echo $result; ?></div>
+                <?php } ?>
 
-    <form class="form-inline" action="<?php echo URL; ?>users/verify" method="post">
-        <div class="form-group mb-2 mr-2">
-            <label for="verify" class="sr-only">Verification Code</label>
-            <input name="verify" type="text" class="form-control" id="verify" placeholder="Verification Code" value="" <?php echo (isset($code) && $code[1] == 'sucessful' ? 'disabled' : 'required'); ?> />
+                <?php if (!isset($code) || $code[1] != 'sucessful') { ?>
+                <form class="form-inline" action="<?php echo URL; ?>users/verify" method="post">
+                    <label for="verify" class="sr-only">Verification Code</label>
+                    <input name="verify" type="text" class="form-control mb-3 mr-1" id="verify" placeholder="Verification Code" value="" required />
+                    <button name="submit_verify_user" type="submit" class="btn btn-primary mb-3">Verify</button>
+                </form>
+                <?php } ?>
+            </div>
         </div>
-        <button name="submit_verify_user" type="submit" class="btn btn-primary mb-2" <?php echo (isset($code) && $code[1] == 'sucessful' ? 'disabled' : ''); ?>>Verify</button>
-    </form>
+    </div>
 </main>
 
