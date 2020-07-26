43e968feefc0c08100bd5fb1214c90611e4e37b3
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-26 14:21:01 -0400

    Autocommit por GitHub.sh: alterações: add 'sql/users.sql'

--- sql/users.sql
+++ sql/users.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS "user" (
+	"id"		INTEGER PRIMARY KEY AUTOINCREMENT,
+	"user"		TEXT UNIQUE,
+	"email"	    TEXT UNIQUE,
+	"role"		TEXT,
+	"password"	TEXT,
+    "temp"      TEXT,
+    "valid"     INTEGER
+);
+
 INSERT OR IGNORE INTO user (id, user, email, role, password, temp, valid) VALUES (1, 'admin', 'admin@google.com', 'admin', '$2y$10$uiPdSuBzk7v4Gf7xb6O9r.w9oVtyh1fKXXl2H/aH/0ulVd3o6YrIa', '{{TEMPID}}', 1);
 INSERT OR IGNORE INTO user (id, user, email, role, password, temp, valid) VALUES (2, 'João', 'joao@facebook.com', 'user', '$2y$10$iT.LbKSuYuuUy/NZn.yRzuV9NVg02hV1FnBTedX4ekFnxcHPKrJTm', '{{TEMPID}}', 1);
 INSERT OR IGNORE INTO user (id, user, email, role, password, temp, valid) VALUES (3, 'José', 'jose@twitter.com', 'user', '$2y$10$iT.LbKSuYuuUy/NZn.yRzuV9NVg02hV1FnBTedX4ekFnxcHPKrJTm', '{{TEMPID}}', 1);
