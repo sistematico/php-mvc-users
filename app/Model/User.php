45e6989ce71edd23097f0ba4d32b8a5d1e7f7cc1
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-26 14:40:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/Model/User.php'

--- app/Model/User.php
+++ app/Model/User.php
@@ -124,7 +124,7 @@ class User extends Model
         while ($row = $query->fetch()) {
             $this->results[] = $row;
         }
-        return (object) $this->result;
+        return (object) $this->results;
     }
 
     public function delete($id)
