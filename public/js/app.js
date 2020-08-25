6fb03d6bb6b712ce4eb8a6d45b7ad33e46a84abe
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-25 00:14:01 -0400

    Autocommit por GitHub: alterações: add 'public/js/app.js'

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    let unix_timestamp = 1549312452
+    var date = new Date(unix_timestamp * 1000);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    //console.log(formattedTime);
+});
