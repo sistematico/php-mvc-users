c27842f94edcb4870881a448f3e7f7e1b4d886f2
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-25 01:44:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/index.php' add 'public/js/app.js'

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 (function() {
-    const unixTimestamp = _ => {
-        let timestamp = parseInt(this.innerHTML);
+    const unixTimestamp = (obj) => {
+        let timestamp = parseInt(obj.innerHTML);
         var date = new Date(timestamp * 1000);
         var hours = date.getHours();
         var minutes = "0" + date.getMinutes();
         var seconds = "0" + date.getSeconds();
         var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-        this.innerHTML = formattedTime;
-        //console.log(this.innerHTML);
+        obj.innerHTML = formattedTime;
+        console.log(formattedTime);
     }
 })();
