46b7ce809a8d767da1571beb3f22635f14781a6f
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-03 08:46:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/index.php'

--- app/view/users/index.php
+++ app/view/users/index.php
@@ -51,13 +51,15 @@
                     <td class="ts"><?php echo (isset($user->created) ? $user->created : 'n/a'); ?></td>
                     <td class="ts"><?php echo (isset($user->access) ? $user->access : 'n/a'); ?></td>
                     <td>
-                        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') { ?>
-                            <a onClick="javascript: return confirm('Are you sure you want to delete?');" href="<?php echo URL . 'users/delete/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-trash"></i></a>
-                            <a href="<?php echo URL . 'users/edit/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-edit"></i></a>
-                            <a href="<?php echo URL . 'users/validate/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-check"></i></a>
-                            <a href="<?php echo URL . 'users/reset/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-undo"></i></a>
-                        <?php } ?>
-                        <a href="<?php echo URL . 'users/profile/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-search"></i></a>
+                        <div class="btn-group" role="group" aria-label="Action Buttons">
+                            <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') { ?>
+                                <a class="btn btn-secondary" onClick="javascript: return confirm('Are you sure you want to delete?');" href="<?php echo URL . 'users/delete/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-trash"></i></a>
+                                <a class="btn btn-secondary" href="<?php echo URL . 'users/edit/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-edit"></i></a>
+                                <a class="btn btn-secondary" href="<?php echo URL . 'users/validate/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-check"></i></a>
+                                <a class="btn btn-secondary" href="<?php echo URL . 'users/reset/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-undo"></i></a>
+                            <?php } ?>
+                            <a class="btn btn-secondary" href="<?php echo URL . 'users/profile/' . htmlspecialchars($user->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-search"></i></a>
+                        </div>
                     </td>
                 </tr>
             <?php } ?>
