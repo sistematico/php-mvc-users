81fe97a9a026f26213993c851712c2a49b931627
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 16:00:01 -0400

    Autocommit por GitHub: alterações: add 'public/js/app.js'

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,19 @@
 (function() {
     const unixTimestamp = (item) => {
         let timestamp = parseInt(item.innerHTML);
-        var date = new Date(timestamp * 1000);
-        var years = date.getFullYear();
-        var months = "0" + (date.getMonth()+1);
-        var days = "0" + (date.getDate()+1);
-        var hours = "0" + date.getHours();
-        var minutes = "0" + date.getMinutes();
-        var seconds = "0" + date.getSeconds();
-        var formattedTime = days.substr(-2) + '/' + months.substr(-2) + '/' + years + ' ' + hours.substr(-2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        let date = new Date(timestamp * 1000);
+        let years = date.getFullYear();
+        let months = "0" + (date.getMonth()+1);
+        let days = "0" + (date.getDate()+1);
+        let hours = "0" + date.getHours();
+        let minutes = "0" + date.getMinutes();
+        let seconds = "0" + date.getSeconds();
+        let formattedTime = days.substr(-2) + '/' + months.substr(-2) + '/' + years + ' ' + hours.substr(-2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
         item.innerHTML = formattedTime;
     }
 
-    var timestamps = document.getElementsByClassName("ts");
-    for (var i = 0; i < timestamps.length; i++) {
+    let timestamps = document.getElementsByClassName("ts");
+    for (let i = 0; i < timestamps.length; i++) {
         unixTimestamp(timestamps[i]);
     }
 })();
