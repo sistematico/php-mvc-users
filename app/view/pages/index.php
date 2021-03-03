acc453218b52fe6093c26c90a52d91a6ade8cae1
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 16:32:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/pages/index.php'

--- app/view/pages/index.php
+++ app/view/pages/index.php
@@ -2,9 +2,7 @@
 
     <nav aria-label="breadcrumb" class="mt-2">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="<?php echo URL; ?>">Home</a></li>
-            <li class="breadcrumb-item">Pages</li>
-            <li class="breadcrumb-item active" aria-current="page">Help</li>
+            <li class="breadcrumb-item active">Home</li>
         </ol>
     </nav>
 
