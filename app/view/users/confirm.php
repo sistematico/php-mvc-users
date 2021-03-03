f4b574b3bdc99a84feb6897bc1fa7cca278e6da1
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 18:42:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/confirm.php'

--- app/view/users/confirm.php
+++ app/view/users/confirm.php
@@ -12,17 +12,16 @@
         <div class="row justify-content-center">
             <div class="col-auto">
                 <h1>Reset password</h1>
-                <?php if (isset($result)) { $code = explode(' ',trim($result)); ?>
-                    <div class="alert alert-primary" role="alert">
-                        <?php echo $result; ?>
-                    </div>
+                <?php if (isset($result->message)) { ?>
+                    <div class="alert alert-primary" role="alert"><?php echo $result->message; ?></div>
                 <?php } ?>
                 <form class="form-inline" action="<?php echo URL; ?>users/verify" method="post">
                     <div class="form-group mb-2 mr-2">
                         <label for="verify" class="sr-only">E-mail</label>
-                        <input name="verify" type="text" class="form-control" id="verify" placeholder="E-mail" value="" <?php echo (isset($code) && $code[1] == 'sucessful' ? 'disabled' : 'required'); ?> />
+                        <input name="verify" type="text" class="form-control" id="verify" placeholder="E-mail" value="" required />
                     </div>
-                    <button name="submit_reset_user" type="submit" class="btn btn-primary mb-2" <?php echo (isset($code) && $code[1] == 'sucessful' ? 'disabled' : ''); ?>>Reset</button>
+                    <button name="submit_reset_user" type="submit" class="btn btn-primary mb-2">Reset</button>
+                    <a href="<?php echo URL; ?>users/login" class="btn btn-success mb-2">Login</a>
                 </form>
             </div>
         </div>
