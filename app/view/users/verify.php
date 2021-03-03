1fc4548638c073df0139f3d836d1d0229e81eeec
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 18:40:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/verify.php'

--- app/view/users/verify.php
+++ app/view/users/verify.php
@@ -1,9 +1,10 @@
-<main role="main" class="container text-center">
+<main role="main" class="container">
 
     <nav aria-label="breadcrumb" class="mt-2" style="--bs-breadcrumb-divider: '>';">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="<?php echo URL; ?>">Home</a></li>
             <li class="breadcrumb-item"><a href="<?php echo URL; ?>users">Users</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo URL; ?>users/login">Login</a></li>
             <li class="breadcrumb-item active" aria-current="page">Verify</li>
         </ol>
     </nav>
@@ -13,21 +14,15 @@
             <div class="col-5">
                 <h1>Verify</h1>
 
-                <?php 
-                    if (isset($result)) { 
-                        $code = explode(' ',trim($result));
-                        //logged
-                ?>
-                    <div class="alert alert-primary" role="alert"><?php echo $result; ?></div>
+                <?php if (isset($result->message)) { ?>
+                    <div class="alert alert-primary" role="alert"><?php echo $result->message; ?></div>
                 <?php } ?>
 
-                <?php if (!isset($code) || $code[1] != 'sucessful') { ?>
                 <form class="form-inline" action="<?php echo URL; ?>users/verify" method="post">
                     <label for="verify" class="sr-only">Verification Code</label>
                     <input name="verify" type="text" class="form-control mb-3 mr-1" id="verify" placeholder="Verification Code" value="" required />
                     <button name="submit_verify_user" type="submit" class="btn btn-primary mb-3">Verify</button>
                 </form>
-                <?php } ?>
             </div>
         </div>
     </div>
