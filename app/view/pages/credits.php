038e26b9f3f3040ea081a774cc894dbfa2689d39
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 18:56:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/pages/credits.php'

--- app/view/pages/credits.php
+++ app/view/pages/credits.php
@@ -15,8 +15,9 @@
             <li><a href="https://www.archlinux.org" target="_blank">Arch Linux</a></li>
             <li><a href="https://php.net" target="_blank">PHP</a></li>
             <li><a href="https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md" target="_blank">PSR-4</a></li>
+            <li><a href="https://www.jetbrains.com/phpstorm/" target="_blank">PhpStorm</a></li>
+            <li><a href="https://getbootstrap.com" target="_blank">Twitter Boostrap 5</a></li>
             <li><a href="https://github.com/panique/mini3" target="_blank">Mini 3</a></li>
-            <li><a href="https://getbootstrap.com" target="_blank">Twitter Boostrap 4</a></li>
             <li><a href="https://en.wikipedia.org/wiki/Faith" target="_blank">Faith</a></li>
         </ul>
     </p>
