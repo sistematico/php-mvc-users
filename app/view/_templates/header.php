93c25e39f450003977a6bdf98fc89b0835e8f7b6
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 18:54:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/_templates/header.php'

--- app/view/_templates/header.php
+++ app/view/_templates/header.php
@@ -18,50 +18,52 @@ $start = $time;
 <body class="d-flex flex-column h-100">
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a class="navbar-brand" href="<?php echo URL; ?>">
-                <img src="<?php echo URL; ?>img/user.svg" width="30" height="30" alt="PHP MVC Users" style="margin-top: -5px;">
-                PHP MVC Users
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="<?php echo URL; ?>">
+                    <img src="<?php echo URL; ?>img/user.svg" width="30" height="30" alt="PHP MVC Users" style="margin-top: -5px;">
+                    PHP MVC Users
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo URL; ?>pages/about"><i class="fas fa-address-card"></i> About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo URL; ?>pages/credits"><i class="fas fa-file"></i> Credits</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo URL; ?>users"><i class="fas fa-users"></i> Users</a>
-                    </li>
-                    <?php if (!isset($_SESSION['logged'])) { ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="fas fa-user-circle"></i> Account
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="<?php echo URL; ?>users/login">Login</a>
-                            <a class="dropdown-item" href="<?php echo URL; ?>users/signup">Signup</a>
-                        </div>
-                    </li>
-                    <?php } else { ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <?php echo (isset($user->avatar)) ? '<img src=' . URL . $user->avatar . ' />' : '<i class="fas fa-user-circle"></i>'; ?> <?php echo (isset($_SESSION['user'])) ? $_SESSION['user'] : 'Account'; ?>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="<?php echo URL; ?>users/logout">Logout</a>
-                        </div>
-                    </li>
-                    <?php } ?>
-                </ul>
-                <form action="<?php echo URL; ?>users/search" method="post" class="d-flex">
-                    <input name="term" class="form-control me-1" type="text" placeholder="Search a user" aria-label="Search">
-                    <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i></button>
-                </form>
+                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo URL; ?>pages/about"><i class="fas fa-address-card"></i> About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo URL; ?>pages/credits"><i class="fas fa-file"></i> Credits</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo URL; ?>users"><i class="fas fa-users"></i> Users</a>
+                        </li>
+                        <?php if (!isset($_SESSION['logged'])) { ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user-circle"></i> Account
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="<?php echo URL; ?>users/login">Login</a>
+                                <a class="dropdown-item" href="<?php echo URL; ?>users/signup">Signup</a>
+                            </div>
+                        </li>
+                        <?php } else { ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?php echo (isset($user->avatar)) ? '<img src=' . URL . $user->avatar . ' />' : '<i class="fas fa-user-circle"></i>'; ?> <?php echo (isset($_SESSION['user'])) ? $_SESSION['user'] : 'Account'; ?>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="<?php echo URL; ?>users/logout">Logout</a>
+                            </div>
+                        </li>
+                        <?php } ?>
+                    </ul>
+                    <form action="<?php echo URL; ?>users/search" method="post" class="d-flex">
+                        <input name="term" class="form-control me-1" type="text" placeholder="Search a user" aria-label="Search">
+                        <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
             </div>
         </nav>
     </header>
