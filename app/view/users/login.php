aa4642b36222ef50ff82b3cc8d6b4469d2b6a18a
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 10:58:02 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/login.php'

--- app/view/users/login.php
+++ app/view/users/login.php
@@ -20,14 +20,14 @@
                 <form action="<?php echo URL; ?>users/login" method="post">
                     <div class="mb-3">
                         <label for="email" class="form-label">E-mail address</label>
-                        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email or login" value="<?php if (isset($_POST['email'])) { echo $_POST['email']; } ?>">
+                        <input name="email" type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email or login" value="<?php if (isset($_POST['email'])) { echo $_POST['email']; } ?>">
                         <div id="emailHelp" class="form-text">
                             We'll never share your email with anyone else.
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input name="password" type="password" class="form-control" id="password" aria-describedby="passwordHelp">
+                        <input name="password" type="password" class="form-control" id="password" aria-describedby="passwordHelp" placeholder="Password" value="<?php if (isset($_POST['password'])) { echo $_POST['password']; } ?>">
                         <small id="passwordHelp" class="form-text text-muted mb-3">
                             Before login, please check your e-mail and/or <a href="<?php echo URL; ?>users/verify">verify</a> your account.
                         </small>
@@ -41,12 +41,6 @@
                     </div>
                     <button name="submit_login_user" type="submit" class="btn btn-lg btn-primary">Sign in</button>
                 </form>
-
-                    <input name="email" type="text" id="email" class="form-control mb-3" placeholder="Email or login" value="<?php if (isset($_POST['email'])) { echo $_POST['email']; } ?>">
-                    <label for="password" class="sr-only">Password</label>
-                    <input name="password" type="password" id="password" class="form-control" placeholder="Password"  value="<?php if (isset($_POST['password'])) { echo $_POST['password']; } ?>">
-
-
             </div>
         </div>
     </div>
