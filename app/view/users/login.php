49e91776e58a3f440e7d822d74f6e6bba1c3fdc1
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 10:52:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/login.php'

--- app/view/users/login.php
+++ app/view/users/login.php
@@ -17,7 +17,24 @@
                     <div class="alert alert-primary" role="alert"><?php echo $result->message; ?></div>
                 <?php } ?>
 
-                <form class="form-signin" action="<?php echo URL; ?>users/login" method="post">
+                <form action="<?php echo URL; ?>users/login" method="post">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail address</label>
+                        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email or login" value="<?php if (isset($_POST['email'])) { echo $_POST['email']; } ?>">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+
                     <label for="email" class="sr-only">Email or Login</label>
                     <input name="email" type="text" id="email" class="form-control mb-3" placeholder="Email or login" value="<?php if (isset($_POST['email'])) { echo $_POST['email']; } ?>">
                     <label for="password" class="sr-only">Password</label>
@@ -36,7 +53,8 @@
                     </div>
 
                     <input name="submit_login_user" type="submit" class="btn btn-lg btn-primary" value="Sign in">
-                </form>
+
+
             </div>
         </div>
     </div>
