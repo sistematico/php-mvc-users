d207a42acef5fb2cce6275be9cdeb7d894a15851
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 10:38:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/_templates/header.php'

--- app/view/_templates/header.php
+++ app/view/_templates/header.php
@@ -9,7 +9,6 @@ $start = $time;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <title>PHP MVC Users</title>
     <link rel="stylesheet" href="<?php echo URL; ?>css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo URL; ?>css/fontawesome.min.css">
