63f034ce0633072a170255b4d87b31496b01ec36
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 12:50:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/pages/credits.php'

--- app/view/pages/credits.php
+++ app/view/pages/credits.php
@@ -12,10 +12,10 @@
     <p class="lead">
         This project was only possible through the help of the following other projects:
         <ul>
+            <li><a href="https://www.jetbrains.com" target="_blank">JetBrains</a> (<a href="https://www.jetbrains.com/phpstorm/" target="_blank">PhpStorm</a>, <a href="https://www.jetbrains.com/webstorm/" target="_blank">WebStorm</a> and <a href="https://www.jetbrains.com/datagrip/" target="_blank">DataGrip</a>)</li>
             <li><a href="https://www.archlinux.org" target="_blank">Arch Linux</a></li>
             <li><a href="https://php.net" target="_blank">PHP</a></li>
             <li><a href="https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md" target="_blank">PSR-4</a></li>
-            <li><a href="https://www.jetbrains.com/phpstorm/" target="_blank">PhpStorm</a></li>
             <li><a href="https://getbootstrap.com" target="_blank">Twitter Boostrap 5</a></li>
             <li><a href="https://github.com/panique/mini3" target="_blank">Mini 3</a></li>
             <li><a href="https://en.wikipedia.org/wiki/Faith" target="_blank">Faith</a></li>
