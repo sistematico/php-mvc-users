30670d01bd750fc5eab6aeac8665296e1dec3c64
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 12:46:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/pages/about.php'

--- app/view/pages/about.php
+++ app/view/pages/about.php
@@ -12,8 +12,9 @@
     <p class="lead">
         This is a simple demonstration project.<br /><br />
         The user with admin role is <span class="font-weight-bold">admin</span> and the password is <span class="font-weight-bold">admin</span>, for other users the password is <span class="font-weight-bold">password</span>.<br /><br />
-        1- Go to: <a href="<?php echo FULL_URL; ?>users/prune"><?php echo FULL_URL; ?>users/prune</a><br />
-        2- Go to: <a href="<?php echo FULL_URL; ?>users/login"><?php echo FULL_URL; ?>users/login</a><br /><br />
+        1- Rename <span class="font-weight-bold">.env.example</span> to <span class="font-weight-bold">.env</span> in project root folder.
+        2- Go to: <a href="<?php echo FULL_URL; ?>users/prune"><?php echo FULL_URL; ?>users/prune</a><br />
+        3- Go to: <a href="<?php echo FULL_URL; ?>users/login"><?php echo FULL_URL; ?>users/login</a><br /><br />
         Thank you and enjoy.
     </p>
 </main>
