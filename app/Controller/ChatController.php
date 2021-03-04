0b830b8db47e265cc8bd1a466f4c9b325fc687be
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 19:12:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/ChatController.php'

--- app/Controller/ChatController.php
+++ app/Controller/ChatController.php
@@ -2,33 +2,23 @@
 
 namespace App\Controller;
 
-class PagesController
+use App\Model\Chat;
+
+class ChatController
 {
     public function index()
     {
+        $Chat = new Chat();
+        $messages = $Chat->list();
+
         require APP . 'view/_templates/header.php';
-        require APP . 'view/pages/index.php';
+        require APP . 'view/chat/list.php';
         require APP . 'view/_templates/footer.php';
     }
 
-    public function about()
+    public function send(string $message)
     {
-        require APP . 'view/_templates/header.php';
-        require APP . 'view/pages/about.php';
-        require APP . 'view/_templates/footer.php';
-    }
-
-    public function credits()
-    {
-        require APP . 'view/_templates/header.php';
-        require APP . 'view/pages/credits.php';
-        require APP . 'view/_templates/footer.php';
-    }
-
-    public function error()
-    {
-        require APP . 'view/_templates/header.php';
-        require APP . 'view/pages/error.php';
-        require APP . 'view/_templates/footer.php';
+        $Chat = new Chat();
+        $Chat->send($message);
     }
 }
