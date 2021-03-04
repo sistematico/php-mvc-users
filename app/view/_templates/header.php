47a89493137ce1927db695e956a9cd45b27c8a0f
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 16:02:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/_templates/header.php'

--- app/view/_templates/header.php
+++ app/view/_templates/header.php
@@ -63,6 +63,7 @@ $start = $time;
                             <?php echo (isset($user->avatar)) ? '<img src=' . URL . $user->avatar . ' />' : '<i class="fas fa-user-circle"></i>'; ?> <?php echo (isset($_SESSION['user'])) ? $_SESSION['user'] : 'Account'; ?>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="<?php echo URL . 'users/profile/' . $user->id; ?>"><i class="fas fa-user-circle"></i> Profile</a>
                                 <a class="dropdown-item" href="<?php echo URL; ?>users/logout"><i class="fas fa-sign-out-alt"></i> Logout</a>
                             </div>
                         </li>
