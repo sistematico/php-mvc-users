bd43888e8d60217ddf657bccb4e1bcb0c59bfd54
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 18:28:02 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/UsersController.php'

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -12,7 +12,7 @@ class UsersController
         $users = $User->list();
         $amount = $User->amount();
         require APP . 'view/_templates/header.php';
-        require APP . 'view/users/index.php';
+        require APP . 'view/users/list.php';
         require APP . 'view/_templates/footer.php';
     }
 
