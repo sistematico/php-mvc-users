31409db259b4b8722a8af19c0fea179898486129
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 15:12:01 -0400

    Autocommit por GitHub: alterações: add 'app/Core/Model.php' add 'db/database.sqlite'

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -23,7 +23,8 @@ class Model
             touch(DB_FILE);
 
         $this->db = new PDO('sqlite:' . DB_FILE);
-        $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        if (MODE === 'development')
+            $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
 }
