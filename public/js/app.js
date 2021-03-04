a8807e61dcd0594287562e56d0b1420baf0f6cbe
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 13:50:01 -0400

    Autocommit por GitHub: alterações: add 'public/js/app.js'

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,16 @@
 (function() {
     const unixTimestamp = (item) => {
         let timestamp = parseInt(item.innerHTML);
-        let date = new Date(timestamp * 1000);
-        let years = date.getFullYear();
-        let months = "0" + (date.getMonth()+1);
-        let days = "0" + (date.getDate()+1);
-        let hours = "0" + date.getHours();
-        let minutes = "0" + date.getMinutes();
-        let seconds = "0" + date.getSeconds();
-        item.innerHTML = days.substr(-2) + '/' + months.substr(-2) + '/' + years + ' ' + hours.substr(-2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        if (typeof timestamp === 'number') {
+            let date = new Date(timestamp * 1000);
+            let years = date.getFullYear();
+            let months = "0" + (date.getMonth() + 1);
+            let days = "0" + (date.getDate() + 1);
+            let hours = "0" + date.getHours();
+            let minutes = "0" + date.getMinutes();
+            let seconds = "0" + date.getSeconds();
+            item.innerHTML = days.substr(-2) + '/' + months.substr(-2) + '/' + years + ' ' + hours.substr(-2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        }
     }
 
     let timestamps = document.getElementsByClassName("ts");
