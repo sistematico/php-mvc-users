99a91e8063e05d44a9d2b089c28be24b727b9fca
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 17:12:02 -0400

    Autocommit por GitHub: alterações: add 'app/view/_templates/header.php' add 'sql/users.sql'

--- sql/users.sql
+++ sql/users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS {{USERS_TABLE}} (
 	"id"		INTEGER PRIMARY KEY AUTOINCREMENT,
 	"user"		TEXT UNIQUE,
+    "avatar"	TEXT,
 	"email"	    TEXT UNIQUE,
 	"role"		TEXT,
 	"password"	TEXT,
