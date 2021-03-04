da2b5b9b2c9849775e1f7145ff3305f3abcc97e2
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 19:08:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/pages/index.php'

--- app/view/pages/index.php
+++ app/view/pages/index.php
@@ -7,5 +7,10 @@
     </nav>
 
     <h1>PHP MVC Users</h1>
-    <p class="lead">Welcome</p>
+    <p class="lead">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        </div>
+    </p>
 </main>
