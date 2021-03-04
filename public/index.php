f6c7574442ad3111958448b83605c388b0d97fc4
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-03 22:40:01 -0400

    Autocommit por GitHub: alterações: add 'public/index.php'

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
 define('DB_FILE', ROOT . 'db' . DIRECTORY_SEPARATOR . 'banco.sqlite');
 define('SQL_FILE', ROOT . 'sql' . DIRECTORY_SEPARATOR . 'database.sql');
 
-require APP . 'config' . DIRECTORY_SEPARATOR . 'autoload.php';
-require APP . 'config' . DIRECTORY_SEPARATOR . 'config.php';
+require APP . DIRECTORY_SEPARATOR . 'autoload.php';
+require APP . DIRECTORY_SEPARATOR . 'config.php';
 
 $app = new Application();
