f497d12ce82d28335f5143ea852f0437893a0d4e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 17:46:02 -0400

    Autocommit por GitHub: alterações: add 'app/view/users/edit.php'

--- app/view/users/edit.php
+++ app/view/users/edit.php
@@ -24,16 +24,16 @@
                     <option value="user" <?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'user') echo 'selected'; ?>>user</option>
                 </select>
             <?php } else { ?>
-                <input name="role" class="form-control" type="text" placeholder="<?php echo isset($user->role) ? htmlspecialchars($user->role, ENT_QUOTES, 'UTF-8') : ''; ?>" readonly>
+                <input name="role" class="form-control" type="text" placeholder="<?php echo isset($user['role']) ? htmlspecialchars($user['role'], ENT_QUOTES, 'UTF-8') : ''; ?>" readonly>
             <?php } ?>
         </div>
         <div class="form-group mb-2 mr-2">
             <label for="login" class="sr-only">Login</label>
-            <input name="login" type="text" class="form-control" id="login" placeholder="Login" value="<?php echo isset($_SESSION['role']) ? htmlspecialchars($user->user, ENT_QUOTES, 'UTF-8') : ''; ?>" required />
+            <input name="login" type="text" class="form-control" id="login" placeholder="Login" value="<?php echo isset($user['role']) ? htmlspecialchars($user['user'], ENT_QUOTES, 'UTF-8') : ''; ?>" required />
         </div>
         <div class="form-group mb-2 mr-2">
             <label for="email" class="sr-only">E-mail</label>
-            <input name="email" type="text" class="form-control" id="email" placeholder="E-mail" value="<?php echo isset($_SESSION['role']) ? htmlspecialchars($user->email, ENT_QUOTES, 'UTF-8') : ''; ?>" required />
+            <input name="email" type="text" class="form-control" id="email" placeholder="E-mail" value="<?php echo isset($user['email']) ? htmlspecialchars($user['email'], ENT_QUOTES, 'UTF-8') : ''; ?>" required />
         </div>
         <div class="form-group mb-2 mr-2">
             <label for="password" class="sr-only">Password</label>
