a505263a0eebec93c82a553be45c9227b30284dc
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 13:00:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/_templates/footer.php' add 'db/database.sqlite'

--- app/view/_templates/footer.php
+++ app/view/_templates/footer.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['last_message'])) {
 
 if (isset($toast)) {
     ?>
-    <div aria-live="polite" aria-atomic="true" class="position-relative">
+    <div aria-live="polite" aria-atomic="true" class="position-relative bottom-0 end-0">
         <div class="toast-container position-absolute p-3 bottom-0 end-0">
             <div class="toast align-items-center border-0 <?php echo $toast['class'] ?? 'text-white bg-dark'; ?>" role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="d-flex">
