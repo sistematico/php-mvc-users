0bb3905dd60d42543a2faaabeef78e75be084a5b
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 20:14:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/chat/list.php'

--- app/view/chat/list.php
+++ app/view/chat/list.php
@@ -1,13 +1,33 @@
 <main class="flex-shrink-0">
     <div class="container">
-        <nav aria-label="breadcrumb" class="mt-2" style="--bs-breadcrumb-divider: '>';">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?php echo URL; ?>">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Chat</li>
-            </ol>
-        </nav>
 
-        <h1>Chat</h1>
+        <div class="row">
+            <div class="col">
+                <nav aria-label="breadcrumb" class="mt-2" style="--bs-breadcrumb-divider: '>';">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?php echo URL; ?>">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Chat</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col">
+                <h1>Chat</h1>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col">
+
+
+
+
+
+
         <div class="card overflow-hidden">
             <div class="card-body overflow-hidden">
                     <?php
@@ -27,5 +47,11 @@
                 </form>
             </div>
         </div>
+
+
+            </div>
+        </div>
+
+
     </div>
 </main>
