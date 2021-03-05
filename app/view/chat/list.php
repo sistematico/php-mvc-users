bd46e26ff68f58989aad0c3a69e734d8f4cff7ab
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-04 21:06:01 -0400

    Autocommit por GitHub: alterações: add 'app/view/chat/list.php'

--- app/view/chat/list.php
+++ app/view/chat/list.php
@@ -20,8 +20,8 @@
             <div class="card-footer">
                 <form action="<?php echo URL; ?>chat/send" method="post">
                     <div class="input-group m-0">
-                        <input name="message" type="text" class="form-control" placeholder="Fale alguma coisa..." aria-label="Fale alguma coisa..." aria-describedby="button-addon" <?php echo isset($_SESSION['logged']) ? '' : 'disabled'; ?>>
-                        <button class="btn btn-outline-secondary" type="submit" id="button-addon">Enviar</button>
+                        <input name="message" type="text" class="form-control" placeholder="<?php echo isset($_SESSION['logged']) ? 'Fale alguma coisa...' : 'Você precisa fazer login para conversar!'; ?>" aria-label="Fale alguma coisa..." aria-describedby="button-addon" <?php echo isset($_SESSION['logged']) ? '' : 'disabled'; ?>>
+                        <button class="btn btn-outline-secondary" type="submit" id="button-addon" <?php echo isset($_SESSION['logged']) ? '' : 'disabled'; ?>>Enviar</button>
                     </div>
                 </form>
             </div>
